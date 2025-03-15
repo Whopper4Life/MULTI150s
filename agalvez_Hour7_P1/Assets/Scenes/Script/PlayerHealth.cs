855eaa6f92bccc9316e77c4ad430f67fcaea76cd
Whopper4Life <yairgalvez95@gmail.com>
2025-03-15 02:08:58 -0700

    Hour 7

---
+++ agalvez_Hour7_P1/Assets/Scenes/Script/PlayerHealth.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float health = 1004f;
+        float poisonDamage = 124.5f;
+        Debug.Log(health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
