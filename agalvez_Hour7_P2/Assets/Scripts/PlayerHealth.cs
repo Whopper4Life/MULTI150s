31d01c3d6be69fcc6869d715e4599ea671632d77
Whopper4Life <yairgalvez95@gmail.com>
2025-03-23 16:48:41 -0700

    Hour7_P2

--- agalvez_Hour7_P2/Assets/Scripts/PlayerHealth.cs
+++ agalvez_Hour7_P2/Assets/Scripts/PlayerHealth.cs
@@ -7,17 +7,12 @@ public class PlayerHealth : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        float health = 1004f;
-        float poisonDamage = 124.5f;
-        Debug.Log(health);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-       while (PlayerHealth> 0)
-            PlayerHealth -= poisonDamage);
-        if (PlayerHealth <= 0)
-            Debug.Log("Player has been unalived");
+        
     }
 }
